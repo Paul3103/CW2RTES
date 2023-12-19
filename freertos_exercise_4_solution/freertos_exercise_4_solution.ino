d0e34a03550ce36518a51c7b6e5fc87e89ea4e12
Paul3103 <plw1g21@soton.ac.uk>
2023-12-19 10:12:17 +0000

    I'm going to hand this in

--- freertos_exercise_4_solution/freertos_exercise_4_solution.ino
+++ freertos_exercise_4_solution/freertos_exercise_4_solution.ino
@@ -4,26 +4,28 @@
 
 
 #define SERIAL_PORT Serial
+#define TASK1_PERIOD_MS 5000
+#define TASK2_PERIOD_MS 10000
+
 
 SemaphoreHandle_t xSemaphore;
-// Function prototypes
-void Task1(void *pvParameters);
-void Task2(void *pvParameters);
+int VeryUsefulSum; //This is going to be added to by Task1 and decremented by Task2
 
 void setup() {
-    // Setup Task 1
-    xSemaphore = xSemaphoreCreateMutex();
-    xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
-    // Setup Task 2
+    //For the actual stuff being done the different tasks add or subtract to the same sum, so it can be seen in the SERIAL port
+    //That the value of VeryUsefulSum ranges from 1 to -1
+    xSemaphore = xSemaphoreCreateMutex();
+    VeryUsefulSum = 0;
+    xTaskCreate(Task1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
     xTaskCreate(Task2, "Task2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
-    // Start the scheduler
-    //vTaskStartScheduler();
+ 
 }
 
 void loop() {
-    // This shouldn't be reached, as the scheduler takes over
+    
 }
 
 void Task1(void *pvParameters) {
@@ -36,16 +38,22 @@ void Task1(void *pvParameters) {
         // Task 1 code here
         xSemaphoreTake(xSemaphore,pdMS_TO_TICKS(1000));
         if(FirstTime){
+        VeryUsefulSum += 1;
+
           SERIAL_PORT.print("Task 1 doing useful things at t=");
           SERIAL_PORT.println(millis()/1000);
-        //SERIAL_PORT.println("THERE IS REASON FOR THIS TO BE DELAYED HAPPEN");
+          SERIAL_PORT.print("The New Value of the very useful sum is: ");
+          SERIAL_PORT.println(VeryUsefulSum);
         FirstTime = !FirstTime;
 
         } else{
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(5000));
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(TASK1_PERIOD_MS));
+        VeryUsefulSum += 1;
 
         SERIAL_PORT.print("Task 1 doing useful things at t=");
         SERIAL_PORT.println(millis()/1000);
+        SERIAL_PORT.print("The New Value of the very useful sum is: ");
+        SERIAL_PORT.println(VeryUsefulSum);
         //SERIAL_PORT.println("THERE IS REASON FOR THIS TO BE DELAYED HAPPEN");
        
         }
@@ -55,17 +63,24 @@ void Task1(void *pvParameters) {
 }
 
 void Task2(void *pvParameters) {
+    int TaskExecTime = 2;
     vTaskDelay(pdMS_TO_TICKS(1000));
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
     while (1) {
         xSemaphoreTake(xSemaphore,pdMS_TO_TICKS(1000));
+        VeryUsefulSum -=1;
+
         SERIAL_PORT.print("Task 2 doing useful things at t=");
         SERIAL_PORT.println(millis()/1000);
-            
+        SERIAL_PORT.print("The New Value of the very useful sum is: ");
+        SERIAL_PORT.println(VeryUsefulSum);           
         
-
+        TaskExecTime -= 1;
+        if (TaskExecTime <= 0) {
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(TASK2_PERIOD_MS));
+          TaskExecTime = 2;
+        }
         // Then waits for the remaining time in the 10-second period
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10000));
     }
 }
